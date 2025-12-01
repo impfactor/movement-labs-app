aabab2ff42ea09beba1d6c1b41fe0e887678b309
IFC <impfactor@gmail.com>
2025-12-02 00:16:37 +0800

    Replace project with proper iOS-only Xcode project

---
+++ ios/MovementLabApp/MovementLabApp/HomeView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Movement Lab")
+                .font(.largeTitle)
+                .padding(.bottom, 8)
+            Text("Basic iOS App Scaffolding")
+                .foregroundColor(.secondary)
+        }
+    }
+}
